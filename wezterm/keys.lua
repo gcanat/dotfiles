fc9f65a78e7d40dcddf74bfb389a5f7ed29f7b91
gcanat <gui.canat@gmail.com>
2024-11-08 15:30:24 +0100

    update wezterm

--- wezterm/keys.lua
+++ wezterm/keys.lua
@@ -1,96 +1,93 @@
 local wezterm = require("wezterm")
 local act = wezterm.action
 return {
-	keys = {
-		{
-			key = "s",
-			mods = "CTRL|ALT",
-			action = act.SplitHorizontal({domain="CurrentPaneDomain"}),
-		},
-		{
-			key = "v",
-			mods = "CTRL|ALT",
-			action = act.SplitVertical({domain="CurrentPaneDomain"}),
-		},
-		{
-			key = "t",
-			mods = "CTRL|SHIFT",
-			action = act.SpawnTab("CurrentPaneDomain"),
-		},
-		{
-			key = "w",
-			mods = "CTRL|SHIFT",
-			action = act.CloseCurrentTab({ confirm = false }),
-		},
-		{
-			key = "c",
-			mods = "CTRL|ALT",
-			action = act.CloseCurrentPane({ confirm = false }),
-		},
-		{
-			key = "LeftArrow",
-			mods = "CTRL|ALT",
-			action = act.ActivatePaneDirection("Left"),
-		},
-		{
-			key = "RightArrow",
-			mods = "CTRL|ALT",
-			action = act.ActivatePaneDirection("Right"),
-		},
-		{
-			key = "UpArrow",
-			mods = "CTRL|ALT",
-			action = act.ActivatePaneDirection("Up"),
-		},
-		{
-			key = "DownArrow",
-			mods = "CTRL|ALT",
-			action = act.ActivatePaneDirection("Down"),
-		},
-		{
-			key = "LeftArrow",
-			mods = "CTRL|SHIFT",
-			action = act.ActivateTabRelative( -1),
-		},
-		{
-			key = "RightArrow",
-			mods = "CTRL|SHIFT",
-			action = act.ActivateTabRelative(1),
-		},
-		-- {
-		-- 	key = "h",
-		-- 	mods = "CTRL|ALT",
-		-- 	action = act.AdjustPaneSize({ "Left", 1 }),
-		-- },
-		-- {
-		-- 	key = "j",
-		-- 	mods = "CTRL|ALT",
-		-- 	action = act.AdjustPaneSize({ "Down", 1 }),
-		-- },
-		-- {
-		-- 	key = "k",
-		-- 	mods = "CTRL|ALT",
-		-- 	action = act.AdjustPaneSize({ "Up", 1 }),
-		-- },
-		-- {
-		-- 	key = "l",
-		-- 	mods = "CTRL|ALT",
-		-- 	action = act.AdjustPaneSize({ "Right", 1 }),
-		-- },
-		{
-			key = "c",
-			mods = "CTRL|SHIFT",
-			action = act.CopyTo("Clipboard"),
-		},
-		{
-			key = "v",
-			mods = "CTRL|SHIFT",
-			action = act.PasteFrom("Clipboard"),
-		},
-		{
-			key = 'f',
-			mods = 'CMD',
-			action = act.ToggleFullScreen,
-		},
-	},
+  keys = {
+    {
+      key = "v",
+      mods = "CTRL|ALT",
+      action = act.SplitHorizontal({domain="CurrentPaneDomain"}),
+    },
+    {
+      key = "s",
+      mods = "CTRL|ALT",
+      action = act.SplitVertical({domain="CurrentPaneDomain"}),
+    },
+    {
+      key = "t",
+      mods = "CTRL|SHIFT",
+      action = act.SpawnTab("CurrentPaneDomain"),
+    },
+    -- CTRL-SHIFT-l activates the debug overlay
+    { key = 'L', mods = 'CTRL', action = wezterm.action.ShowDebugOverlay },
+    {
+      key = "w",
+      mods = "CTRL|SHIFT",
+      action = act.CloseCurrentTab({ confirm = false }),
+    },
+    {
+      key = "c",
+      mods = "CTRL|ALT",
+      action = act.CloseCurrentPane({ confirm = false }),
+    },
+    {
+      key = "h",
+      mods = "CTRL|ALT",
+      action = act.ActivatePaneDirection("Left"),
+    },
+    {
+      key = "l",
+      mods = "CTRL|ALT",
+      action = act.ActivatePaneDirection("Right"),
+    },
+    {
+      key = "k",
+      mods = "CTRL|ALT",
+      action = act.ActivatePaneDirection("Up"),
+    },
+    {
+      key = "j",
+      mods = "CTRL|ALT",
+      action = act.ActivatePaneDirection("Down"),
+    },
+    {
+      key = "h",
+      mods = "CTRL|SHIFT",
+      action = act.ActivateTabRelative( -1),
+    },
+    {
+      key = "l",
+      mods = "CTRL|SHIFT",
+      action = act.ActivateTabRelative(1),
+    },
+    {
+      key = "c",
+      mods = "CTRL|SHIFT",
+      action = act.CopyTo("Clipboard"),
+    },
+    {
+      key = "v",
+      mods = "CTRL|SHIFT",
+      action = act.PasteFrom("Clipboard"),
+    },
+    {
+      key = 'f',
+      mods = 'CMD',
+      action = act.ToggleFullScreen,
+    },
+    {
+      key = '=',
+      mods = 'CTRL',
+      action = act.ResetFontSize,
+    },
+    {
+      key = '-',
+      mods = 'CTRL',
+      action = act.DecreaseFontSize,
+    },
+    {
+      key = '+',
+      mods = 'CTRL|SHIFT',
+      action = act.IncreaseFontSize,
+    },
+  },
 }
--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,141 +1,139 @@
 local wezterm = require("wezterm")
 local k = require("keys")
--- local b = require("background")
 
--- The filled in variant of the < symbol
-local SOLID_LEFT_ARROW = wezterm.nerdfonts.pl_right_hard_divider
--- The filled in variant of the > symbol
-local SOLID_RIGHT_ARROW = wezterm.nerdfonts.pl_left_hard_divider
+local config = {}
+config.inactive_pane_hsb = {
+  saturation = 0.7,
+  brightness = 0.5,
+}
 
--- local wezterm = require 'wezterm'
+config.webgpu_preferred_adapter = {
+  backend = 'Vulkan',
+  device = 8081,
+  device_type = 'DiscreteGpu',
+  driver = 'NVIDIA',
+  driver_info = '535.183.01',
+  name = 'NVIDIA GeForce GTX 1650',
+  vendor = 4318,
+}
 
--- The filled in variant of the < symbol
--- local SOLID_LEFT_ARROW = utf8.char(0xe0b2)
+config.colors = {
+  tab_bar = {
+    -- The color of the strip that goes along the top of the window
+    -- (does not apply when fancy tab bar is in use)
+    background = '#0b0022',
+    inactive_tab_edge = '#575757',
 
--- The filled in variant of the > symbol
--- local SOLID_RIGHT_ARROW = utf8.char(0xe0b0)
+    -- The active tab is the one that has focus in the window
+    active_tab = {
+      -- The color of the background area for the tab
+      bg_color = '#2b2042',
+      -- The color of the text for the tab
+      fg_color = '#c0c0c0',
 
-wezterm.on(
-  'format-tab-title',
-  function(tab, tabs, panes, config, hover, max_width)
-    local edge_background = '#2b2042'
-    local background = '#24273a'
-    local foreground = '#808080'
+      -- Specify whether you want "Half", "Normal" or "Bold" intensity for the
+      -- label shown for this tab.
+      -- The default is "Normal"
+      intensity = 'Normal',
 
-    if tab.is_active then
-      background = '#3b3052'
-      foreground = '#c0c0c0'
-    elseif hover then
-      background = '#3b3052'
-      foreground = '#909090'
-    end
+      -- Specify whether you want "None", "Single" or "Double" underline for
+      -- label shown for this tab.
+      -- The default is "None"
+      underline = 'None',
 
-    local edge_foreground = background
+      -- Specify whether you want the text to be italic (true) or not (false)
+      -- for this tab.  The default is false.
+      italic = false,
 
-    -- ensure that the titles fit in the available space,
-    -- and that we have room for the edges.
-    local title = wezterm.truncate_right(tab.active_pane.title, max_width - 2)
+      -- Specify whether you want the text to be rendered with strikethrough (true)
+      -- or not for this tab.  The default is false.
+      strikethrough = true,
+    },
 
-    return {
-      { Background = { Color = edge_background } },
-      { Foreground = { Color = edge_foreground } },
-      { Text = SOLID_LEFT_ARROW },
-      { Background = { Color = background } },
-      { Foreground = { Color = foreground } },
-      { Text = title },
-      { Background = { Color = edge_background } },
-      { Foreground = { Color = edge_foreground } },
-      { Text = SOLID_RIGHT_ARROW },
-    }
-  end
-)
+    -- Inactive tabs are the tabs that do not have focus
+    inactive_tab = {
+      bg_color = '#1b1032',
+      fg_color = '#808080',
+    },
 
--- display lag when using multiplexing functionality
-wezterm.on('update-status', function(window, pane)
-  local meta = pane:get_metadata() or {}
-  if meta.is_tardy then
-    local secs = meta.since_last_response_ms / 1000.0
-    window:set_right_status(string.format('tardy: %5.1fs⏳', secs))
-  end
-end)
+    -- You can configure some alternate styling when the mouse pointer
+    -- moves over inactive tabs
+    inactive_tab_hover = {
+      bg_color = '#3b3052',
+      fg_color = '#909090',
+      italic = true,
+    },
+
+    -- The new tab button that let you create new tabs
+    new_tab = {
+      bg_color = '#1b1032',
+      fg_color = '#808080',
+    },
+
+    -- You can configure some alternate styling when the mouse pointer
+    -- moves over the new tab button
+    new_tab_hover = {
+      bg_color = '#3b3052',
+      fg_color = '#909090',
+      italic = true,
+    },
+  },
+}
 
 return {
-	term = "xterm-kitty",
-	font = wezterm.font_with_fallback {
-    -- "DejaVu Sans Mono",
-    "Input Mono Condensed",
-    { family = "Symbols Nerd Font Mono", scale = 0.8},
-    "Noto Color Emoji"
+  front_end = "WebGpu",
+  -- term = "wezterm",
+  font = wezterm.font_with_fallback { "Fantasque Sans Mono", "Symbols Nerd Font Mono" },
+  font_size = 17,
+  hide_tab_bar_if_only_one_tab = false,
+  use_fancy_tab_bar = false,
+  tab_bar_at_bottom = false,
+  show_new_tab_button_in_tab_bar = false,
+  enable_scroll_bar = true,
+  scrollback_lines = 50000,
+  use_cap_height_to_scale_fallback_fonts = true,
+  -- color_scheme = 'GruvboxDarkHard',
+  color_scheme = 'Kanagawa (Gogh)',
+  colors = config.colors,
+  disable_default_key_bindings = true,
+  inactive_pane_hsb = config.inactive_pane_hsb,
+  force_reverse_video_cursor = true,
+  colors = {
+    scrollbar_thumb = "gray",
   },
-	-- font = wezterm.font("VictorMono Nerd Font Mono"),
-	-- font = wezterm.font("FiraCode Nerd Font Mono"),
-	-- font = wezterm.font("Iosevka Term SS09"),
-	-- font = wezterm.font("JetBrains Mono"),
-	-- font = wezterm.font("DinaRemasterII"),
-	-- font = wezterm.font("Terminus (TTF)"),
-	-- font = wezterm.font("Terminus"),
-	font_size = 10.5,
-	hide_tab_bar_if_only_one_tab = true,
-	-- use_fancy_tab_bar = false,
-	tab_bar_at_bottom = true,
-	enable_scroll_bar = true,
-	scrollback_lines = 50000,
-	disable_default_key_bindings = true,
-	color_scheme = "Catppuccin Macchiato", -- or Macchiato, Frappe, Latte
-	animation_fps = 1,
-  cursor_blink_rate = 0,
-  cursor_blink_ease_in = "Constant",
-  cursor_blink_ease_out = "Constant",
-	colors = {
-		scrollbar_thumb = "gray",
-	},
-	window_padding = {
-    	left = "0cell",
-    	right = "0cell",
-    	top = "0cell",
-    	bottom = "0cell",
-  	},
-	-- background = b.background,
-	keys = k.keys,
-	ssh_domains = {
-		{
-			name = 'huff',
-	    remote_address = 'huff_remote',
-	    username = 'root',
-	  },
-		{
-			name = 'sly',
-	    remote_address = 'sly_remote',
-	    username = 'root',
-	  },
-	},
-	window_frame = {
-		-- font = wezterm.font("DejaVu Sans Mono"),
-    font = wezterm.font("Input Mono Condensed"),
-		font_size = 9,
-		inactive_titlebar_bg = "#2b2042",
-		active_titlebar_bg = "#2b2042",
-	},
-	-- tab_bar_style = {
-	--   active_tab_left = wezterm.format {
-	--     { Background = { Color = '#0b0022' } },
-	--     { Foreground = { Color = '#2b2042' } },
-	--     { Text = SOLID_LEFT_ARROW },
-	--   },
-	--   active_tab_right = wezterm.format {
-	--     { Background = { Color = '#0b0022' } },
-	--     { Foreground = { Color = '#2b2042' } },
-	--     { Text = SOLID_RIGHT_ARROW },
-	--   },
-	--   inactive_tab_left = wezterm.format {
-	--     { Background = { Color = '#0b0022' } },
-	--     { Foreground = { Color = '#1b1032' } },
-	--     { Text = SOLID_LEFT_ARROW },
-	--   },
-	--   inactive_tab_right = wezterm.format {
-	--     { Background = { Color = '#0b0022' } },
-	--     { Foreground = { Color = '#1b1032' } },
-	--     { Text = SOLID_RIGHT_ARROW },
-	-- 		},
-	-- 	},
-	}
+  window_padding = {
+      left = "0cell",
+      right = "0cell",
+      top = "0cell",
+      bottom = "0cell",
+    },
+  -- background = b.background,
+  keys = k.keys,
+  ssh_domains = {
+    {
+      name = 'ttkap',
+      remote_address = '136.243.23.68:15050',
+      username = 'ttkap',
+      multiplexing = 'None',
+    },
+  },
+  window_frame = {
+    font = wezterm.font("Fantasque Sans Mono"),
+    font_size = 18,
+    inactive_titlebar_bg = "#2b2042",
+    active_titlebar_bg = "#2b2042",
+  },
+  window_background_opacity = 0.8,
+  -- window_background_image = '/home/guillaume/Pictures/wallpaper.jpg',
+  --   window_background_image_hsb = {
+  --   -- Darken the background image by reducing it to 1/3rd
+  --   brightness = 0.2,
+
+  --   -- You can adjust the hue by scaling its value.
+  --   -- a multiplier of 1.0 leaves the value unchanged.
+  --   hue = 0.1,
+
+  --   -- You can adjust the saturation also.
+  --   saturation = 0.3,
+  --   }
+  }
